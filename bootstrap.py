4adb27a616bc6d2f1665ad383bba8185a11fa9e0
davidbestue <davidsanchezbestue@hotmail.com>
2019-09-06 17:36:20 +0200

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -45,7 +45,31 @@ def bootstrap(data, num_samples, statistic, alpha):
 
 
     
-#    
+#
+
+
+
+
+
+def boots_by_subj(data, col_int, col_subj, n_iterations, alpha, stat):
+    #### you give a 2 column df, one column qith the value and the other column with subject index:
+    list_subjects = data[col_subj].unique()
+    sample=[]
+    for n in range(n_iterations):
+        resampled=[]
+        new_sample = list(np.random.randint(0, len(list_subjects), len(list_subjects)))
+        for res_s in new_sample:
+            resampled = resampled + list(data.loc[data[col_subj]==list_subjects[res_s], col_int].values) 
+        #
+        sample.append(stat(resampled))
+    #
+    stats_sorted = np.sort(sample)
+    new_mean=np.mean(sample)
+    return (new_mean, stats_sorted[int((alpha/2.0)*n_iterations)],
+            stats_sorted[int((1-alpha/2.0)*n_iterations)])
+
+
+    
 #
 ### Example
 #As = [np.random.rand(1000) for i in range(0,100)]
