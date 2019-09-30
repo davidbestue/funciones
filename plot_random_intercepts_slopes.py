0401bd15801f284472230e6089e4e32729ddabc7
davidbestue <davidsanchezbestue@hotmail.com>
2019-09-30 15:01:07 +0200

    Update plot_random_intercepts_slopes.py

--- plot_random_intercepts_slopes.py
+++ plot_random_intercepts_slopes.py
@@ -1,6 +1,9 @@
 
 
 
+
+
+
 def plot_rand_int(data, x, y, hue, summary=True): 
     pal = sns.color_palette("tab10", n_colors=len(list(data[hue].unique())), desat=1).as_hex()
     sns.lmplot(x, y, data,  hue=hue, hue_order=list(data[hue].unique()),
@@ -21,4 +24,28 @@ def plot_rand_int(data, x, y, hue, summary=True):
     if summary==True:
         print(mdf.summary())
 
-        
+
+
+
+
+def plot_rand_slope(data, x, y, hue, summary=True): 
+    pal = sns.color_palette("tab10", n_colors=len(list(data[hue].unique())), desat=1).as_hex()
+    sns.lmplot(x, y, data,  hue=hue, hue_order=list(data[hue].unique()),
+               fit_reg=False, legend=False, size=8, palette=pal)
+    md = smf.mixedlm(y + '~' + x, data, groups=hue, re_formula='~' + x)
+    mdf = md.fit()
+    ### 
+    intercept_add_each= np.array([mdf.random_effects[N][hue] for N in list(data[hue].unique())])
+    slope_each = np.array([mdf.random_effects[N][x] for N in list(data[hue].unique())])    
+    intercept_each_subj =  intercept_add_each + mdf.params.Intercept
+    slope_each_subj =  slope_each + mdf.params[x]
+    
+    for i, ind in enumerate(list(data[hue].unique())):        
+        intercept_s=intercept_each_subj[i]
+        slope_s = slope_each_subj[i]
+        start_l = data[x].min()
+        end_l = data[x].max()
+        plt.plot([start_l, end_l], [slope_s*start_l + intercept_s, slope_s*end_l + intercept_s], color=pal[i])
+    
+    if summary==True:
+        print(mdf.summary())
