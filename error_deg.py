66aa188755158caf6fedc514b2a0efc1f23fb590
davidbestue <davidsanchezbestue@hotmail.com>
2020-01-29 15:07:22 +0100

    update

--- error_deg.py
+++ error_deg.py
@@ -21,8 +21,6 @@ def err_rad(a1,ref):
     ### Calculate the error ref-a1 in an efficient way in the circular space
     ### it uses complex numbers!
     ### Input in radians (0-2pi)
-    a1=np.radians(a1)
-    ref=np.radians(ref)
-    err = np.angle(np.exp(1j*ref)/np.exp(1j*(a1) ), deg=True) 
+    err = np.angle(np.exp(1j*ref)/np.exp(1j*(a1) ), deg=False) 
     err=round(err, 2)
     return err
